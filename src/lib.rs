e8af6373a2a832ea85f75699eef66e5b21b91818
Akshay Shedbalkar <akshayshedbalkar@gmail.com>
2026-04-26 18:27:35 +0200

    Prevent panic

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ pub fn run() {
     let c = Config::new();
     let mut a: Api = Api::new(&c);
     a.fetch();
-    println!("{:#?}", a.data[0].meta_data);
-    println!("{:#?}", a.data[0].historical_data.last_entry().unwrap());
+    if !a.data.is_empty()
+    {
+        println!("{:#?}", a.data[0].meta_data);
+        println!("{:#?}", a.data[0].historical_data.last_entry().unwrap());
+    }
 }
